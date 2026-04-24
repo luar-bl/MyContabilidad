bc73610465627003b8be92cd6a88cf0de120f189
Blanco <rblanco.jira@gmail.com>
2026-04-24 19:40:50 +0200

    ProyectoCasa. Nueva página con charts

---
+++ Components/Pages/Charts/Pag_Mo_Charts.razor.cs
@@ -0,0 +1,9 @@
+﻿namespace ProyectoCasa.Components.Pages.Charts
+{
+    public partial class Pag_Mo_Charts
+    {
+
+
+
+    }
+}
---
+++ Service/Charts/ServicioCharts.cs
@@ -0,0 +1,20 @@
+﻿namespace ProyectoCasa.Service.Charts
+{
+    public class ServicioCharts
+    {
+
+
+        public ServicioCharts()
+        {
+
+        }
+
+        public async Task<List<object>> ObtenerCasaYGastosTotal()
+        {
+            
+
+        }
+
+
+    }
+}
